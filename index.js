7787284d7c412364d40c70133174f3d45385d6f8
BrandonDiazMT <brandon.diaz.mt@usa.edu.co>
2022-12-16 22:57:53 -0500

    Versión 1.0

---
+++ index.js
@@ -0,0 +1,100 @@
+const btnCart = document.querySelector('.container-cart-icon')
+const containerCartProducts = document.querySelector('.container-cart-products')
+
+btnCart.addEventListener('click', () => {
+    containerCartProducts.classList.toggle('hidden-cart')
+})
+
+/* ************************************** */ 
+
+const cartInfo = document.querySelector('cart-product')
+const rowProduct = document.querySelector('.row-product')
+
+// Lista de todos los contenedores del carrito
+
+const productList = document.querySelector('.container-items')
+
+// Variable de arreglo de productos
+
+let allProducts = []
+
+
+
+
+
+
+productList.addEventListener('click', e => {
+
+    if(e.target.classList.contains('btn-add-cart')){
+        const product = e.target.parentElement
+        
+        const infoProduct = {
+            quantity:1,
+            tittle: product.querySelector('h2').textContent,
+            price: product.querySelector('p').textContent
+        };
+
+        const exits = allProducts.some(product => product.title === infoProduct.title)
+        
+        if (exits){
+            const products = allProducts.map(product =>{
+                if(product.title === infoProduct.title){
+                    product.quantity++;
+                    return product
+                }else{
+                    return product
+                }
+            })
+            allProducts = [...products];
+        }else{
+            allProducts = [...allProducts, infoProduct];
+        }
+        
+
+
+        
+        
+        showHTML();
+    }
+
+    
+})
+
+// Función para mostrar HTML
+
+const showHTML =() =>{
+
+//Limpiar HTML
+
+    rowProduct.innerHTML = ''
+
+    //quedamos AQUÍ ---> Minuto 30:00
+
+    allProducts.forEach(product => {
+        const containerProduct = document.createElement('div')
+        containerProduct.classList.add('cart-product')
+        
+        containerProduct.innerHTML = `
+            <div class="info-cart-product"><>
+                <span class="cantidad-producto-carrito">${product.quantity}</span>
+                <p class="titulo-producto-carrito">${product.tittle}</p>
+                <span class="precio-producto-carrito">${product.price}</span></>
+            </div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="icon-close"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                />
+            </svg>
+            `
+        rowProduct.append(containerProduct);
+    })
+}
